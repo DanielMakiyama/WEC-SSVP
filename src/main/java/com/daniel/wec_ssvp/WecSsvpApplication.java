4f7cff5c8ac9ff7a39ca0eafba0f8b4a6aef5fcf
Daniel Shizuo <danielmakiyamap@gmail.com>
2026-03-16 19:01:04 -0300

    feat: criação da árvore de pastas

---
+++ src/main/java/com/daniel/wec_ssvp/WecSsvpApplication.java
@@ -0,0 +1,13 @@
+package com.daniel.wec_ssvp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WecSsvpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WecSsvpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/daniel/wec_ssvp/WecSsvpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel.wec_ssvp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WecSsvpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
