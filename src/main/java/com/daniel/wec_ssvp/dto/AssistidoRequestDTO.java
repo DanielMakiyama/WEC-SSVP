5942e1f5cbd6ad129f96abeb72ae81938be9e52f
Daniel Shizuo <danielmakiyamap@gmail.com>
2026-03-23 18:59:22 -0300

    feat: implementa padrão DTO

---
+++ src/main/java/com/daniel/wec_ssvp/dto/AssistidoRequestDTO.java
@@ -0,0 +1,27 @@
+package com.daniel.wec_ssvp.dto;
+
+import jakarta.persistence.Column;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public record AssistidoRequestDTO(
+        String nome,
+        String conjuge,
+        String endereco,
+        LocalDate dataNascimento,
+        String estadoCivil,
+        String religiao,
+        String situacaoMoradia,
+        String profissao,
+        Integer quantidadeTrabalhadores,
+        BigDecimal rendaFamiliar,
+        BigDecimal rendaLiquida,
+        BigDecimal valorAluguel,
+        Integer quantidadeAlfabetizados,
+        String situacaoCatequeseCrisma,
+        String participacaoIgrejaCatolica,
+        String problemaSaude,
+        String outrasInformacoes
+) {
+}
---
+++ src/main/java/com/daniel/wec_ssvp/dto/AssistidoResponseDTO.java
@@ -0,0 +1,25 @@
+package com.daniel.wec_ssvp.dto;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public record AssistidoResponseDTO(
+        String nome,
+        String conjuge,
+        String endereco,
+        LocalDate dataNascimento,
+        String estadoCivil,
+        String religiao,
+        String situacaoMoradia,
+        String profissao,
+        Integer quantidadeTrabalhadores,
+        BigDecimal rendaFamiliar,
+        BigDecimal rendaLiquida,
+        BigDecimal valorAluguel,
+        Integer quantidadeAlfabetizados,
+        String situacaoCatequeseCrisma,
+        String participacaoIgrejaCatolica,
+        String problemaSaude,
+        String outrasInformacoes
+){
+}
